d1504c075b18584e81457cfe0dc9ea43a153cc6c
Du0ngVanBa0 <tidumong789@gmail.com>
2024-05-20 22:58:22 +0700

    add database context

---
+++ FinalEx/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinalEx.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
