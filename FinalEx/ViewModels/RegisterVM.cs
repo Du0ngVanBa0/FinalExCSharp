06854488e8f16d06fa78828bf5be6e6edeb20d80
Du0ngVanBa0 <tidumong789@gmail.com>
2024-05-21 21:38:08 +0700

    add auth Controller, login partial and auth view model

---
+++ FinalEx/ViewModels/RegisterVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalEx.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Compare("Password", ErrorMessage = "Password isn't match.")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+        [Required]
+        public string? Name { get; set; }
+    }
+}
